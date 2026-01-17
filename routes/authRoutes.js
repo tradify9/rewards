d0b7ef4a9e7d5d22e098650151f9008de9a9bd4b
Ganesh R Prajapati <ganeshrprajapati29@gmail.com>
2026-01-17 19:38:09 +0530

    latest push

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,11 +6,7 @@ const { protect } = require('../middleware/authMiddleware');
 // Multer setup for file uploads
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.post('/register', upload.fields([
-  { name: 'aadhaar', maxCount: 1 },
-  { name: 'pancard', maxCount: 1 },
-  { name: 'photo', maxCount: 1 }
-]), register);
+router.post('/register', register);
 router.post('/login', login);
 router.get('/profile', protect, getProfile);
 router.post('/forgot-password', forgotPassword);
