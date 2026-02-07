0329edd9f9deb957caef707af85bda51313c461c
Ganesh Prajapati <ganeshrprajapati29@gmail.com>
2026-02-07 15:21:17 +0530

    latets push

--- server.js
+++ server.js
@@ -400,7 +400,7 @@ app.use((err, req, res, next) => {
 /* -------------------------------
    Start AFTER DB connect
 -------------------------------- */
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
